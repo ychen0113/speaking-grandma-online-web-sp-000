1ae4e7d89c10b893c9e751f800fff4d4f2976e37
Yiliang Chen <yiliangchen0113@gmail.com>
2020-09-27 18:37:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif words != words.upcase && words != "I LOVE YOU GRANDMA!"
-  puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   else 
-  puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end 
 end
